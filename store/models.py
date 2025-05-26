30a71de5c9bcc96b5706fea3a1f5b5d725d1f901
Moetaz <motaz062002@gmail.com>
2025-05-26 17:24:36 +0100

    User cart

--- store/models.py
+++ store/models.py
@@ -35,6 +35,17 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    @property
+    def get_total_cart(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.SET_NULL)
@@ -44,6 +55,10 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return str(self.product)
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
     
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, null=True, blank=True, on_delete=models.SET_NULL)
--- store/views.py
+++ store/views.py
@@ -8,10 +8,24 @@ def store(request):
 
 
 def cart(request):
-    context={}
+    if request.user.is_authenticated:
+        Customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=Customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_total_cart': 0, 'get_cart_items': 0}
+    context={"items": items,"order": order}
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    context={}
+    if request.user.is_authenticated:
+        Customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=Customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_total_cart': 0, 'get_cart_items': 0}
+    context = {"items": items, "order": order}
     return render(request, 'store/checkout.html', context)
